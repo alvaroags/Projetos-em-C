fbefae0e5e34481a30e556fdf29481d38cfe75c0
alvaroags <alvarogsilva.neto@gmail.com>
2022-03-04 19:11:55 -0300

    Update salario2.c

--- salario2.c
+++ salario2.c
@@ -3,8 +3,7 @@
 int main(){
     char nome;
     double fixo, salario;
-    scanf("%s %lf", nome, &fixo);
+    scanf("%s %lf", &nome, &fixo);
     salario = fixo * 0.15 + fixo;
     printf("TOTAL = R$ %.2lf", salario);
-    return 0;
 }
